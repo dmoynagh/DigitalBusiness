03c9c1fb743e1f4efb155be0abaa9a9456d6f457
dmoynagh <david@electrolink.co.nz>
2026-07-27 23:43:03 +1200

    wip

---
+++ Projects/DigitalBusiness.DependencyInjectionExtensions/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace DigitalBusiness.DependencyInjectionExtensions
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Tests/DigitalBusiness.DependencyInjectionExtensions.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace DigitalBusiness.DependencyInjectionExtensions.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
